76afc353c226ad9d9f9aed1120b4229a9be2b0f0
869288142 <869288142@qq.com>
2019-05-11 14:49:30 +0800

    归并排序

---
+++ mergeSort.js
@@ -0,0 +1,48 @@
+let arr = []
+for (let i = 0; i < 20; i++) {
+  let e = Math.floor(Math.random() * 110)
+  arr.push(e)
+}
+
+// 归并排序
+// 利用分治法将问题分解成更小的子问题，然后将子问题结果合并
+function mergeSort(
+  arr,
+  left = 0,
+  right = arr.length - 1,
+  temp = new Array(arr.length)
+) {
+  if(left < right) {
+    let mid = parseInt(left + (right - left) / 2)
+    mergeSort(arr, left, mid, temp)
+    mergeSort(arr, mid + 1, right, temp)
+    merge(arr, left, mid, right, temp)
+  }
+}
+function merge(arr, left, mid, right, temp) {
+  let i = left
+  let j = mid + 1
+  let t = 0 //临时数组指针
+  while (i <= mid && j <= right) {
+    if (arr[i] <= arr[j]) {
+      temp[t++] = arr[i++]
+    } else {
+      temp[t++] = arr[j++]
+    }
+  }
+  while (i <= mid) {
+    //将左边剩余元素填充进temp中
+    temp[t++] = arr[i++]
+  }
+  while (j <= right) {
+    //将右序列剩余元素填充进temp中
+    temp[t++] = arr[j++]
+  }
+  t = 0
+  //将temp中的元素全部拷贝到原数组中
+  while (left <= right) {
+    arr[left++] = temp[t++]
+  }
+}
+mergeSort(arr)
+console.log(arr);
